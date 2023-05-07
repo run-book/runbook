79511668a592ca4dcc7e8a42d1e6e006f861a238
phil <phil.rice@validoc.org>
2023-05-07 09:37:12 +0200

    Adding playground to allow us to play with runbook

--- code/modules/playground/npx/index.ts
+++ code/modules/playground/npx/index.ts
@@ -7,8 +7,11 @@ import { ping, status } from "@runbook/apistatus";
 const port = process.argv[ 2 ] ? Number.parseInt ( process.argv[ 2 ] ) : 3001
 
 console.log ( 'port', port )
-startKoa ( process.cwd (), port, defaultHandler ( status ( '/api', () => ({}) ), ping ( "/api" ) ) ).then ( () => {
-  if ( osType () === 'Windows_NT' ) {
-    executeScriptInShell ( process.cwd (), `start http://localhost:${port}/api/status` )
-  }
-} )
+startKoa ( process.cwd (), port, defaultHandler (
+  status ( '/api', () => ({}) ),
+  ping ( "/api" ) ) )
+  .then ( () => {
+    if ( osType () === 'Windows_NT' ) {
+      executeScriptInShell ( process.cwd (), `start http://localhost:${port}/api/status` )
+    }
+  } )
