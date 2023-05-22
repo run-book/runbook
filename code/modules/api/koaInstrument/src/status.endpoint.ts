8cb99b360502b63eaf132790560f5b4a92ea707e
phil <phil.rice@validoc.org>
2023-05-22 22:46:32 +0200

    v0.3.1 & improving status endpoint to have exit code

--- code/modules/api/koaInstrument/src/status.endpoint.ts
+++ code/modules/api/koaInstrument/src/status.endpoint.ts
@@ -1,7 +1,7 @@
 import { KoaPartialFunction } from "@runbook/koa";
 import { Execution, StatusEndpointData } from "@runbook/executors";
 import { Cache } from "@runbook/cache";
-import { mapObjValues, NameAnd, Primitive } from "@runbook/utils";
+import { mapObjValues, mapObjValuesK, NameAnd, Primitive } from "@runbook/utils";
 
 
 export function executeStatusEndpoint<T> ( path: string, cache: Cache<Execution<[ string, T ]>> ): KoaPartialFunction {
@@ -11,12 +11,19 @@ export function executeStatusEndpoint<T> ( path: string, cache: Cache<Execution<
       return result;
     },
     apply: async ( { context } ) => {
-      const status: NameAnd<StatusEndpointData> = mapObjValues ( cache, ( { cached, count, lastUpdated } ) => {
-        const name = cached.t[ 0 ];
-        const params = cached.params;
+      const status: NameAnd<StatusEndpointData> = await mapObjValuesK ( cache, async ( { cached: execution, count, lastUpdated } ) => {
+        const name = execution.t[ 0 ];
+        const params = execution.params;
 
-        const finished = cached.finished;
-        return { name, finished, count, lastUpdated, params };
+        const finished = execution.finished;
+        // console.log ( 'execution', execution )
+        if ( finished && execution.promise ) {
+          const p = await execution.promise
+          console.log('p', p)
+
+          return { name, finished, count, lastUpdated, params, code: p.code, output: p.output };
+        } else
+          return { name, finished, count, lastUpdated, params };
       } );
       context.body = JSON.stringify ( status )
       context.status = 200;
